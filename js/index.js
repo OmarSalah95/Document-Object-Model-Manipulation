c01566fe27e4d2c11a5f743ec29f3faffa0b864e
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-01-29 14:00:41 -0800

    Initial commit and PR. I have made progress. Added 2 selectors used to visibly modify the dom using event listeners.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
-// Your code goes here
+// // Your code goes here
+// * [ ] `keydown`
+// * [ ] `wheel`
+// * [ ] `drag / drop`
+// * [ ] `load`
+// * [ ] `focus`
+// * [ ] `resize`
+// * [ ] `scroll`
+// * [ ] `select`
+// * [ ] `dblclick`
+
+// Nav event listener to nest a second click listener in 
+const nav = document.querySelector('nav');
+nav.addEventListener('click', e => console.log(e.target));
+
+// Removes Default action for click event of Anchor tags using a second nested click listener with propagation control. 
+const anchors = Array.from(document.getElementsByTagName('a'));
+anchors.forEach(anchor => {
+    anchor.addEventListener(`click`, e => {
+        console.log(e.target);
+        e.preventDefault(); // This prevents elements default action to triggering, so no page refresh.
+        e.stopPropagation() // This prevents propagation. 
+    });
+    // Applies to For each on Anchor tags.
+    });
+
+// Enlarges images on MouseOver
+const images = Array.from(document.querySelectorAll('p'));
+images.forEach(img =>{
+    img.addEventListener(`mouseover`, e => {
+    e.target.style.backgroundColor = 'grey';
+    });
+    // Applies to For each on Anchor tags.
+    });
