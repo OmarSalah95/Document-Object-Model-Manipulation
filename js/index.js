7f0d2605e7372f1cf569ba902d03856d8c6d6bd9
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-01-29 15:24:55 -0800

    Lots of progress on MVP

--- js/index.js
+++ js/index.js
@@ -1,13 +1,4 @@
 // // Your code goes here
-// * [ ] `keydown`
-// * [ ] `wheel`
-// * [ ] `drag / drop`
-// * [ ] `load`
-// * [ ] `focus`
-// * [ ] `resize`
-// * [ ] `scroll`
-// * [ ] `select`
-// * [ ] `dblclick`
 
 // Nav event listener to nest a second click listener in 
 const nav = document.querySelector('nav');
@@ -25,10 +16,31 @@ anchors.forEach(anchor => {
     });
 
 // Enlarges images on MouseOver
-const images = Array.from(document.querySelectorAll('p'));
-images.forEach(img =>{
-    img.addEventListener(`mouseover`, e => {
-    e.target.style.backgroundColor = 'grey';
+const pTags = Array.from(document.querySelectorAll('p'));
+pTags.forEach(p =>{
+    // On mouseover BGColor change
+    p.addEventListener(`mouseover`, e => {
+        e.target.style.backgroundColor = 'grey';
     });
+    // On mouseleave BGColor return
+    p.addEventListener(`mouseleave`, e => {
+        e.target.style.backgroundColor = 'white'
+    });
+    // Zoom in on dblclick
+    p.addEventListener(`dblclick`, e => {
+        e.target.style.fontSize = '20px'
+    });
+    p.addEventListener("copy", event => (p.style.color = "red"));
     // Applies to For each on Anchor tags.
-    });
+    p.style.borderRadius = "5px"
+    p.style.padding = "5px"
+});
+
+
+const body = document.querySelector("body");
+body.addEventListener("keydown", e =>
+  alert("You can NOT modify the page content!")
+);
+body.addEventListener("loadeddata", e =>{
+    TweenMax.to(e.target, 1, {rotation:720, yoyo: true, repeat: 1});
+});
